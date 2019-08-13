88a0791d3a11987090170b6cf87c9a0638125724
NickGraham101 <nick.graham101@googlemail.com>
2019-08-13 21:43:35 +0100

    Dot sourced missing class in Get-Release tests

--- Tests/U010-Get-Release.Tests.ps1
+++ Tests/U010-Get-Release.Tests.ps1
@@ -2,6 +2,7 @@ Push-Location -Path $PSScriptRoot\..\
 
 Describe "Get-Release unit tests" -Tag "Unit" {
     
+    . .\VstsTools\Classes\ReleaseEnvironment.ps1
     . .\VstsTools\Functions\Private\Invoke-VstsRestMethod.ps1
 
     $SharedParams = @{
