e9ac9eda7da684d0b8df66b9aea9f24585a4ac90
NickGraham101 <nick.graham101@googlemail.com>
2019-09-06 19:16:51 +0100

    Revert "Fixed scope"
    This reverts commit 5fae6f24b6f9afec51c0247890e667c477d1fc7b.

--- Tests/Invoke-Tests.ps1
+++ Tests/Invoke-Tests.ps1
@@ -26,7 +26,7 @@ Param (
 )
 
 Install-Module -Name Pester -Scope CurrentUser
-Import-Module Pester -Scope Local
+Import-Module Pester -Scope CurrentUser
 
 $TestParameters = @{
     OutputFormat = 'NUnitXml'
