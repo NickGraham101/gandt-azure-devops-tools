c99ff55c98de68dfc86fb6e4e267027236e9b08d
NickGraham101 <nick.graham101@googlemail.com>
2019-09-06 18:53:30 +0100

    Install Pester

--- Tests/Invoke-Tests.ps1
+++ Tests/Invoke-Tests.ps1
@@ -25,6 +25,9 @@ Param (
     [String] $CodeCoveragePath
 )
 
+Install-Module -Name Pester -Scope CurrentUser
+Import-Module Pester -Scope CurrentUser
+
 $TestParameters = @{
     OutputFormat = 'NUnitXml'
     OutputFile   = "$PSScriptRoot\TEST-$TestType.xml"
