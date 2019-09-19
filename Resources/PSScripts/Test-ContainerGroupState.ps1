f87edf3292ed890e2a67d2b99cd5438f1f2640ea
NickGraham101 <nick.graham101@googlemail.com>
2019-09-19 14:47:14 +0100

    Added ContainerImageName as a param

--- Resources/PSScripts/Test-ContainerGroupState.ps1
+++ Resources/PSScripts/Test-ContainerGroupState.ps1
@@ -1,6 +1,8 @@
 [CmdletBinding()]
 param(
     [Parameter(Mandatory=$true)]
+    [String]$ContainerImageName,
+    [Parameter(Mandatory=$true)]
     [String]$ContainerName,
     [Parameter(Mandatory=$true)]
     [String]$ResourceGroupName
@@ -9,14 +11,16 @@ param(
 $ContainerGroup = Get-AzContainerGroup -ResourceGroupName $ResourceGroupName -Name $ContainerName
 Write-Verbose "Container Group with Name $($ContainerGroup.Name) retrieved"
 
-if ($ContainerGroup.Containers[0].Image -eq $($(ContainerRegistryAdminUser).azurecr.io/$(AgentImageName))) { 
+if ($ContainerGroup.Containers[0].Image -eq $ContainerImageName) { 
     
+    Write-Verbose "Setting ImageNeedsUpdating to 'true'"
     Write-Output "##vso[task.setvariable variable=ImageNeedsUpdating]true" 
 
 }
 
 if ($ContainerGroup.State -ne "Running") { 
     
+    Write-Verbose "Setting ContainerNeedsStarting to 'true'"
     Write-Output "##vso[task.setvariable variable=ContainerNeedsStarting]true" 
 
 }
