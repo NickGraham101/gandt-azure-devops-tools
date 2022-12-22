faee8ac67b8e0006268ae175a2f1bb5b295672dc
NickGraham101 <nick.graham101@googlemail.com>
2022-12-22 10:31:51 +0000

    Added test for Merge-MultiplePullRequest

---
+++ Tests/U024-Merge-MultiplePullRequest.Tests.ps1
@@ -0,0 +1,55 @@
+Push-Location -Path $PSScriptRoot\..\
+Import-Module "$PSScriptRoot/../gandt-azure-devops-tools/gandt-azure-devops-tools.psm1" -Force
+
+Describe "Merge-MultiplePullRequest unit tests" -Tag "Unit" {
+
+    Mock Get-PullRequest -ModuleName gandt-azure-devops-tools -MockWith {
+        return New-Object -TypeName PullRequest -Property @{
+            PullRequestId = "123"
+            SourceBranchRef = "refs/heads/foo"
+            LastMergeSourceCommit = "0000000000000000000000000000000000000001"
+        }
+    }
+    Mock Get-PullRequestPolicyEvaluation -ModuleName gandt-azure-devops-tools -MockWith {
+        return New-Object -TypeName PullRequestPolicyEvaluation -Property @{
+            Status = "approved"
+        }
+    }
+    Mock New-Branch -ModuleName gandt-azure-devops-tools -MockWith {
+        return New-Object -TypeName Branch -Property @{
+            Name =  "Bar"
+            CommitId = "0000000000000000000000000000000000000001"
+        }
+    }
+    Mock New-Merge -ModuleName gandt-azure-devops-tools -MockWith {
+        return New-Object -TypeName Branch
+    }
+    Mock Close-PullRequest -ModuleName gandt-azure-devops-tools
+    Mock New-PullRequest -ModuleName gandt-azure-devops-tools -MockWith {
+        return New-Object -TypeName PullRequest -Property @{
+            PullRequestId = "124"
+            Title = "This merges the pull requests"
+        }
+    }
+
+    ##. .\gandt-azure-devops-tools\Functions\Public\Combined\Merge-MultiplePullRequest.ps1
+
+    Context "some test" {
+        It "Will return a PullRequest object" {
+            $SharedParams = @{
+                Instance = "notarealinstance"
+                PatToken = "not-a-real-token"
+                ProjectId = "notarealproject"
+                RepositoryId = "1234"
+                MergedPullRequestBranchName = "FOO-4321"
+            }
+
+            $TestParams = $SharedParams
+
+            $Output = Merge-MultiplePullRequest @SharedParams
+            $Output.GetType().Name | Should Be "PullRequest"
+            #$Output.SourceBranchRef | Should Be "refs/head/foo"
+        }
+    }
+
+}
