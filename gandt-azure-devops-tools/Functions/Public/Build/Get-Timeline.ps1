db64919c73dd126f221a57badb76e4a87ba89ff9
NickGraham101 <nick.graham101@googlemail.com>
2023-11-30 21:21:05 +0000

    Add Get-Timeline cmdlet

---
+++ gandt-azure-devops-tools/Functions/Public/Build/Get-Timeline.ps1
@@ -0,0 +1,57 @@
+<#
+    .NOTES
+    API Reference: https://learn.microsoft.com/en-us/rest/api/azure/devops/build/timeline/get?view=azure-devops-rest-7.1
+#>
+function Get-Timeline {
+    [CmdletBinding()]
+    param (
+        #The Visual Studio Team Services account name
+        [Parameter(Mandatory=$true)]
+        [string]$Instance,
+
+        #A PAT token with the necessary scope to invoke the requested HttpMethod on the specified Resource
+        [Parameter(Mandatory=$true)]
+        [string]$PatToken,
+
+        #Parameter Description
+        [Parameter(Mandatory=$true)]
+        [string]$ProjectId,
+
+        #Parameter Description
+        [Parameter(Mandatory=$true, ParameterSetName="Id")]
+        [int]$BuildId
+    )
+
+    process {
+
+        $GetTimelineParams = @{
+            Instance = $Instance
+            PatToken = $PatToken
+            Collection = $ProjectId
+            Area = "build"
+            Resource = "builds"
+            ResourceId = $BuildId
+            ResourceComponent = "timeline"
+            ApiVersion = "7.1-preview.2"
+        }
+
+        $TimelineJson = Invoke-AzDevOpsRestMethod @GetTimelineParams
+
+        $Timeline = New-TimelineObject -BuildId $BuildId -TimelineJson $TimelineJson
+        $Timeline
+    }
+}
+
+function New-TimelineObject {
+    param (
+        $BuildId,
+        $TimelineJson
+    )
+
+    $Timeline = New-Object -TypeName Timeline
+    $Timeline.BuildId = $BuildId
+    $Timeline.FailedJobs = ($TimelineJson.records | Where-Object {$_.Type -eq "Job" -and $_.Result -eq "failed"}) -ne $null ? $true : $false
+    $Timeline.FailedStages = ($TimelineJson.records | Where-Object {$_.Type -eq "Stage" -and $_.Result -eq "failed"}) -ne $null ? $true : $false
+    $Timeline.FailedTasks = ($TimelineJson.records | Where-Object {$_.Type -eq "Task" -and $_.Result -eq "failed"}) -ne $null ? $true : $false
+    $Timeline
+}
